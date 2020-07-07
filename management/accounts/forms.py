23196944b6740e545d4c95de7693cf6a822adb48
Pranav Jandu <pranav.jandu@gmail.com>
2020-07-07 14:11:57 +0530

    back to square 1

--- management/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-
-class CreateUserModel(UserCreationForm):
-    class Meta:
-        model=User
-        fields=['username','email','password1','password2']
